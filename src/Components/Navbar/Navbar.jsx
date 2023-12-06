e9f240782fbac4ec9535c6687acdc1806be973d2
Santhosh7401 <santhoshkumars7401@gmail.com>
2023-12-06 23:38:21 +0530

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
+import {  } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+
+ 
 
 const Navbar =()=>{
+
+    const [menu,setMenu]=useState("Shop")
     return(
         <div className="navbar">
             <div className="nav-logo">
                 <img src="" alt=""/>
-                <p>Shopper</p>
+                <p>PERK</p>
 
             </div>
 
             <ul className="nav-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li onClick={()=>{setMenu("shop")}}> Home {menu==="shop"?<hr/>:<></>} </li>
+                <li onClick={()=>{setMenu("mens")}}> Menu {menu==="mens"?<hr/>:<></>} </li>
+                <li onClick={()=>{setMenu("womens")}}> About {menu==="womens"?<hr/>:<></>} </li>
+                <li onClick={()=>{setMenu("kids")}}> Contact {menu==="kids"?<hr/>:<></>} </li>
             </ul>
 
             <div className="login-cart">
+                <h1><IoCartOutline /></h1>
+                <div className="cart-count">0</div>
                 <button>Login</button>
-                <img src="" alt=""></img>
+                
             </div>
 
         </div>
